342e872db01ecb38b46fd6d7447af5810f67407c
BogdanKozlov35 <bkozlov35@gmail.com>
2024-03-22 16:20:12 +0200

    task 2

--- HW_task_05_01.py
+++ HW_task_05_01.py
@@ -1,26 +1,25 @@
 # ФУНКЦІЯ caching_fibonacci
-from collections import defaultdict
 def caching_fibonacci():
-    cache = defaultdict(int)
+    cache = {0: 0, 1: 1}
     def fibonacci(n):
 
-        if n <= 0:
-            return 0
-        if n ==1:
-            return 1
         if n in cache:
             return cache[n]
         else:
             cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
-            #print(cache)
             return cache[n]
     return fibonacci
-fib_func = caching_fibonacci()
+def main ():
 
-print(fib_func(10))
-print(fib_func(15))
-print(fib_func(20))
-print(fib_func(100))
-print(fib_func(0))
-print(fib_func(1))
+    fib_func = caching_fibonacci()
+
+    print(fib_func(5))
+    print(fib_func(10))
+    print(fib_func(15))
+    print(fib_func(20))
+    print(fib_func(0))
+    print(fib_func(1))
+
+if __name__ == "__main__":
+    main()
 
