0340eea3e0ae1fb2afe837c3a4af4990081a335f
BogdanKozlov35 <bkozlov35@gmail.com>
2024-03-22 16:42:15 +0200

    task 2

--- HW_task_05_02.py
+++ HW_task_05_02.py
@@ -1,13 +1,21 @@
 import re
 
 def generator_numbers(text: str):  # пошук сумм в тексті
-    yield from re.findall(r"\d+\.\d+", text)
+    yield from re.findall(r"\d+\.\d{1,2}", text)
 def sum_profit(text: str, func: generator_numbers): # обчислення загального доходу після пошуку всіх сум
     numbers = func(text) #  результат пошуку
 
     total = sum(float(num) for num in numbers)
     return total
 
-text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
-total_income = sum_profit(text, generator_numbers)
-print(f"Загальний дохід: {total_income}")
+def main ():
+    text = ("""Загальний дохід працівника складається
+    з декількох частин: 1000.01 як основний дохід, 
+    доповнений додатковими надходженнями 27.45 і 324.00 доларів"""
+    )
+    total_income = sum_profit(text, generator_numbers)
+    print(f"Загальний дохід: {total_income}")
+
+
+if __name__ == "__main__":
+     main()
